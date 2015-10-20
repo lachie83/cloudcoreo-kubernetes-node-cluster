ffe266f4660550b0529fb744de0e961bf831f270
Paul Allen <pauldallen@gmail.com>
2015-10-20 09:29:42 +0200

    fixing english

--- boot-scripts/install_docker.sh
+++ boot-scripts/install_docker.sh
@@ -5,31 +5,31 @@ yum install -y docker
 source /etc/profile.d/cluster
 
 name="$(echo $MY_IPADDRESS | perl -pe 's{\.}{}g')"
-all_nets="$(python $PWD/lib/generate_network_blocks.py  --master-cidr-block ${KUBE_MASTER_SERVICE_IP_CIDRS} --cidr-divider ${KUBE_MASTER_SERVICE_IP_CIDRS_SUBDIVIDER})"
+all_nets="$(python ./lib/generate_network_blocks.py  --master-cidr-block ${KUBE_MASTER_SERVICE_IP_CIDRS} --cidr-divider ${KUBE_MASTER_SERVICE_IP_CIDRS_SUBDIVIDER})"
 
-## get us an unused bip
+## get an unused bip
 (
-kube_dir="/opt/kubernetes"
-cd "$kube_dir"
-used_nets="$(./kubectl --server=http://${KUBE_MASTER_NAME}.${DNS_ZONE}:8080 get nodes | grep ipblock | awk -F'ipblock=' '{print $2}' | perl -pe 's#([0-9\.]+).*#\1#g')"
+    kube_dir="/opt/kubernetes"
+    cd "$kube_dir"
+    used_nets="$(./kubectl --server=http://${KUBE_MASTER_NAME}.${DNS_ZONE}:8080 get nodes | grep ipblock | awk -F'ipblock=' '{print $2}' | perl -pe 's#([0-9\.]+).*#\1#g')"
 
-DOCKER_BIP=
-for aNet in $all_nets; do
-    unused=true
-    for uNet in $used_nets; do
-	if [ "$uNet" = "$aNet" ]; then
-	    unused=false
+    DOCKER_BIP=
+    for aNet in $all_nets; do
+	unused=true
+	for uNet in $used_nets; do
+	    if [ "$uNet" = "$aNet" ]; then
+		unused=false
+	    fi
+	done
+	
+	if [ $unused = true ]; then
+	    DOCKER_BIP="$aNet"
+	    break
 	fi
     done
-
-    if [ $unused = true ]; then
-	DOCKER_BIP="$aNet"
-	break
-    fi
-done
-
-## docker config
-cat <<EOF > /etc/sysconfig/docker
+    
+    ## docker config
+    cat <<EOF > /etc/sysconfig/docker
 # The max number of open files for the daemon itself, and all
 # running containers.  The default value of 1048576 mirrors the value
 # used by the systemd service unit.
